faf05b95b55fb9a15d829fb99ea1ec1906fb8c70
Ivan Panarusky <eviltechie@joe.to>
2013-03-30 03:58:18 -0400

    Rewrite newpart command to allow console use

--- src/main/java/to/joe/strangeweapons/command/NewPartCommand.java
+++ src/main/java/to/joe/strangeweapons/command/NewPartCommand.java
@@ -8,45 +8,68 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
 import to.joe.strangeweapons.Part;
+import to.joe.strangeweapons.Util;
+import to.joe.strangeweapons.exception.BadPlayerMatchException;
 import to.joe.strangeweapons.meta.StrangePart;
 
 public class NewPartCommand implements CommandExecutor {
 
+    /*
+     * part
+     * part quantity
+     * part quantity target
+     */
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        if (!(sender instanceof Player)) {
-            sender.sendMessage(ChatColor.RED + "Only players may use this command");
-            return true;
-        }
-        if (args.length < 1) {
-            sender.sendMessage(ChatColor.RED + "/newpart <partname>");
-            return true;
-        }
-        Part chosenPart;
-        try {
-            chosenPart = Part.valueOf(args[0].toUpperCase());
-        } catch (IllegalArgumentException e) {
-            sender.sendMessage(ChatColor.RED + "Invalid part");
-            return true;
-        }
         int quantity = 1;
+        Part chosenPart;
+        Player target;
+
+        if (args.length > 0) {
+            try {
+                chosenPart = Part.valueOf(args[0].toUpperCase());
+            } catch (IllegalArgumentException e) {
+                sender.sendMessage(ChatColor.RED + "Invalid part");
+                return true;
+            }
+        } else {
+            sender.sendMessage(ChatColor.RED + "Specify a part name");
+            return true;
+        }
+
         if (args.length > 1) {
             try {
                 quantity = Integer.parseInt(args[1]);
-                if (quantity < 1) {
-                    quantity = 1;
-                }
-                if (quantity > 64) {
-                    quantity = 64;
-                }
             } catch (NumberFormatException e) {
                 sender.sendMessage(ChatColor.RED + "That's not a number");
                 return true;
             }
+            if (quantity < 1) {
+                quantity = 1;
+            } else if (quantity > 64) {
+                quantity = 64;
+            }
         }
+
+        if (args.length > 2) {
+            try {
+                target = Util.getPlayer(args[3], sender);
+            } catch (BadPlayerMatchException e) {
+                sender.sendMessage(ChatColor.RED + e.getMessage());
+                return true;
+            }
+        } else {
+            if (sender instanceof Player) {
+                target = (Player) sender;
+            } else {
+                sender.sendMessage(ChatColor.RED + "Specify a player to give the part to");
+                return true;
+            }
+        }
+
         ItemStack part = new StrangePart(chosenPart).getItemStack();
         part.setAmount(quantity);
-        ((Player) sender).getInventory().addItem(part);
+        target.getInventory().addItem(part);
         sender.sendMessage(ChatColor.GOLD + "Given " + ChatColor.AQUA + quantity + ChatColor.GOLD + " strange part(s) of type \"" + ChatColor.AQUA + chosenPart.getName() + ChatColor.GOLD + "\"");
         return true;
     }
