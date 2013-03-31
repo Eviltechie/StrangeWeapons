a4fe6a340f8e4703c092ca91f07b71225d6f01b5
Ivan Panarusky <eviltechie@joe.to>
2013-03-31 00:56:49 -0400

    Split out destruction checks

--- src/main/java/to/joe/strangeweapons/listener/IncrementListener.java
+++ src/main/java/to/joe/strangeweapons/listener/IncrementListener.java
@@ -3,24 +3,18 @@ package to.joe.strangeweapons.listener;
 import java.util.Map.Entry;
 
 import org.bukkit.ChatColor;
-import org.bukkit.Material;
 import org.bukkit.entity.Arrow;
 import org.bukkit.entity.Player;
-import org.bukkit.entity.PoweredMinecart;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
-import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
-import org.bukkit.event.player.PlayerInteractEntityEvent;
-import org.bukkit.inventory.ItemStack;
 
 import to.joe.strangeweapons.Part;
 import to.joe.strangeweapons.StrangeWeapons;
 import to.joe.strangeweapons.Util;
-import to.joe.strangeweapons.meta.Crate;
 import to.joe.strangeweapons.meta.StrangeWeapon;
 
 public class IncrementListener implements Listener {
@@ -92,18 +86,6 @@ public class IncrementListener implements Listener {
         }
     }
 
-    @SuppressWarnings("deprecation")
-    @EventHandler
-    public void onInteract(PlayerInteractEntityEvent event) {
-        Player p = event.getPlayer();
-        ItemStack item = event.getPlayer().getItemInHand();
-        if (event.getRightClicked() instanceof PoweredMinecart && item.getType() == Material.COAL && StrangeWeapon.isStrangeWeapon(p.getItemInHand())) {
-            event.setCancelled(true);
-            p.updateInventory();
-            p.sendMessage(ChatColor.RED + "You may not use that in a powered minecart.");
-        }
-    }
-
     @EventHandler
     public void onEntityDeath(EntityDeathEvent event) {
         if (event.getEntity().getKiller() != null) {
@@ -127,17 +109,6 @@ public class IncrementListener implements Listener {
                 }
                 p.setItemInHand(item.getItemStack());
             }
-        }
-    }
-
-    @EventHandler
-    public void onBlockPlace(BlockPlaceEvent event) {
-        if (Crate.isCrate(event.getItemInHand())) {
-            event.setCancelled(true);
-            event.getPlayer().sendMessage(ChatColor.RED + "You may not place Steve Co. Supply Crates");
-        } else if (event.getItemInHand().getType().isBlock() && StrangeWeapon.isStrangeWeapon(event.getItemInHand())) {
-            event.setCancelled(true);
-            event.getPlayer().sendMessage(ChatColor.RED + "You may not place strange weapons");
         }
     }
 }
