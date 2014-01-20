4c1091a9072185652b0b888da83c2aab7e18ca31
Ivan Panarusky <eviltechie@joe.to>
2014-01-20 02:45:03 -0500

    some work on sw class

--- src/main/java/to/joe/strangeweapons/meta/StrangeWeapon.java
+++ src/main/java/to/joe/strangeweapons/meta/StrangeWeapon.java
@@ -1,10 +1,41 @@
 package to.joe.strangeweapons.meta;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.bukkit.configuration.serialization.ConfigurationSerializable;
+import org.bukkit.configuration.serialization.ConfigurationSerialization;
 import org.bukkit.inventory.ItemStack;
 
-public class StrangeWeapon {
+import to.joe.strangeweapons.Part;
+import to.joe.strangeweapons.Quality;
 
-    public static boolean isStrangeWeapon(ItemStack item) {
-        return false; //TODO
+public class StrangeWeapon implements ConfigurationSerializable {
+
+    static {
+        ConfigurationSerialization.registerClass(StrangeWeapon.class);
     }
+
+    private String name;
+    private String description;
+    private Quality quality;
+    private Map<Part, Integer> parts;
+
+    public StrangeWeapon(ItemStack item) {
+        //TODO
+    }
+
+    public StrangeWeapon(Map<String, Object> map) {
+        //TODO
+    }
+
+    @Override
+    public Map<String, Object> serialize() { //TODO
+        Map<String, Object> map = new LinkedHashMap<String, Object>();
+        map.put("name", name);
+        map.put("description", description);
+        map.put("quality", quality.toString());
+        return map;
+    }
+
 }
