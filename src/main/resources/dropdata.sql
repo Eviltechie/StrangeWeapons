7490185cb8ed1c3e1f6f47e4a7f7da89611175a4
Ivan Panarusky <eviltechie@joe.to>
2014-05-04 00:49:06 -0400

    i meant everything

--- src/main/resources/dropdata.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS `dropdata` (
-  `username` varchar(16) NOT NULL,
-  `playtime` int(6) NOT NULL,
-  `nextitemdrop` int(6) NOT NULL,
-  `nextcratedrop` int(6) NOT NULL,
-  PRIMARY KEY (`username`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
--- src/main/resources/droprecords.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS `droprecords` (
-  `username` varchar(16) NOT NULL,
-  `itemdropped` varchar(512) NOT NULL,
-  `iscrate` tinyint(1) NOT NULL,
-  `when` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
